1a7798668f2690b8fcc6c499ea15a1dd76be579d
HWAN <dltmdghks543@gmail.com>
2024-07-06 20:52:03 +0900

    2024-07-06

---
+++ src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+
+    public static void main(String[] args) {
+        int age = 18;
+        String status = (age >= 18) ? "성인" : "미성년자";
+        System.out.println("age = " + age + " status = " + status);
+    }
+
+}
